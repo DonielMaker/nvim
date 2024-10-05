806e09c19f967a3be1eae8bd190700810a5afdf5
donielmaker <daniel.schmidt0204@gmail.com>
2024-10-05 18:27:00 +0200

    Fixed init.lua files.

--- lua/settings/init.lua
+++ lua/settings/init.lua
@@ -1,12 +1,4 @@
 require("settings.lazy")
 require("settings.options")
+require("settings.windows")
 
-local keymap = vim.keymap.set
-
-keymap("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
-
-keymap("n", "<leader>tv", "<C-w>v", { desc = "Split window vertically"})
-keymap("n", "<leader>th", "<C-w>s", { desc = "Split window horizontally"})
-keymap("n", "<leader>tx", "<cmd>close<CR>", { desc = "Close current window"})
-
-keymap("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal"})
