99a25309db180f606097daa434689b3b721a2acd
donielmaker <daniel.schmidt0204@gmail.com>
2024-10-05 18:29:02 +0200

    Renamed vim-tmux-navigator.lua to vim-tmux-navigator.lua.bak| moved window related settings from lua/settings/init.lua to lua/settings/windows.lua| Moved vim-tmux-navigator logic to lua/settings/windows.lua| Now using vim movement instead of vim-tmux-navigator.lua movement

---
+++ lua/settings/windows.lua
@@ -0,0 +1,20 @@
+local keymap = vim.keymap.set
+
+keymap("n", "<leader>ch", ":nohl<CR>", { desc = "Clear search highlights" })
+
+keymap("n", "<leader>tv", "<C-w>v", { desc = "Split window vertically"})
+keymap("n", "<leader>th", "<C-w>s", { desc = "Split window horizontally"})
+keymap("n", "<leader>tx", "<C-w>q", { desc = "Close current window"})
+
+keymap("n", "<C-Up>", "<C-w>k", { desc = "Navigate Up"})
+keymap("n", "<C-Down>", "<C-w>j", { desc = "Navigate Down"})
+keymap("n", "<C-Left>", "<C-w>h", { desc = "Navigate Left"})
+keymap("n", "<C-Right>", "<C-w>l", { desc = "Navigate Right"})
+
+keymap("n", "<S-Up>", "<C-w>+", { desc = "Increase window height"})
+keymap("n", "<S-Down>", "<C-w>-", { desc = "Decrease window height"})
+keymap("n", "<S-Left>", "<C-w>>", { desc = "Increase window width"})
+keymap("n", "<S-Right>", "<C-w><", { desc = "Decrease window width"})
+
+keymap("n", "<leader>tt", "<cmd>ToggleTerm<CR>", { desc = "Toggle Terminal"})
+
