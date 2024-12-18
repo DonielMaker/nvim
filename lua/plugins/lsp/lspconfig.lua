4eb5bc9bfc56ea5b7b5b0ddd40365d54a6a68b9e
donielmaker <daniel.schmidt0204@gmail.com>
2024-12-18 14:59:14 +0100

    fixed lspconfig stuff

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -17,10 +17,18 @@ return {
                 vim.fn.sign_define(hl, { text = icon, texthl = hl, numhl = "" })
             end
 
-            lspconfig.lua_ls.setup{capabilitites}
-            lspconfig.rust_analyzer.setup{capabilitites}
-            lspconfig.nil_ls.setup({capabilitites})
-            lspconfig.ts_ls.setup({capabilitites})
+            local lsp_servers = {
+                "lua_ls",
+                "rust_analyzer",
+                "nil_ls",
+                "ts_ls",
+                "cssls",
+                "bashls",
+            }
+
+            for _, lsp in ipairs(lsp_servers) do
+                lspconfig[lsp].setup{capabilitites}
+            end
         end
     }
 }
