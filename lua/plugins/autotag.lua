f0e8e2ab4a1f3b9ff153e280b03b9af8f788216d
donielmaker <daniel.schmidt0204@gmail.com>
2024-12-21 10:12:58 +0100

    removed unused plugins; fixed some cmp stuff

--- lua/plugins/autotag.lua
+++ lua/plugins/autotag.lua
@@ -1,9 +1,7 @@
 return {
     {
         'windwp/nvim-ts-autotag',
-        dependencies = {
-            'nvim-treesitter/nvim-treesitter',
-        },
+        dependencies = { 'nvim-treesitter/nvim-treesitter' },
 
         config = function()
             local autotag = require('nvim-ts-autotag')
--- lua/plugins/better_comments.lua
+++ lua/plugins/better_comments.lua
@@ -5,16 +5,15 @@ return {
         dependencies = { "nvim-lua/plenary.nvim" },
         config = function()
             local todo_comments = require("todo-comments")
+            -- local keymap = vim.keymap.set
 
-            local keymap = vim.keymap.set
-
-            keymap("n", "]t", function()
-                todo_comments.jump_next()
-            end, { desc = "Next todo comment" })
-
-            keymap("n", "[t", function()
-                todo_comments.jump_prev()
-            end, { desc = "Previous todo comment" })
+            -- keymap("n", "]t", function()
+            --     todo_comments.jump_next()
+            -- end, { desc = "Next todo comment" })
+            --
+            -- keymap("n", "[t", function()
+            --     todo_comments.jump_prev()
+            -- end, { desc = "Previous todo comment" })
 
             todo_comments.setup()
         end,
