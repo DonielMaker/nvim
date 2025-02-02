d975abbc5b95efab9132b2ec321205ab4856a8dc
donielmaker <daniel.schmidt0204@gmail.com>
2025-02-02 14:16:12 +0100

    renamed completion.lua to blink.lua

--- lua/plugins/blink.lua
+++ lua/plugins/blink.lua
@@ -1,6 +1,7 @@
 return {
     {
         'saghen/blink.cmp',
+        dependencies = 'rafamadriz/friendly-snippets',
         version = 'v0.*',
 
         opts = {
@@ -8,12 +9,22 @@ return {
                 ["<Up>"] = {"select_prev", "fallback"},
                 ["<Down>"] = {"select_next", "fallback"},
                 ["<C-Space>"] = {"show", "show_documentation", "hide_documentation"},
-                ["<C-CR>"] = {"select_and_accept", "fallback"},
+                ["<Tab>"] = {"select_and_accept", "fallback"},
+                -- ["<C-CR>"] = {"select_and_accept", "fallback"},
             },
 
             appearance = {
-                use_nvim_cmp_as_default = true,
+                -- use_nvim_cmp_as_default = true,
                 nerd_font_variant = 'normal'
+            },
+
+            completion = {
+                documentation = {
+                    auto_show = true,
+                    auto_show_delay_ms = 500,
+                },
+
+                ghost_text = {enabled = true,},
             },
 
             signature = { enabled = true }
