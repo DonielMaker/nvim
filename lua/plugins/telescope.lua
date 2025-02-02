308488193a318225fcb92e50f4ffc53346aac216
donielmaker <daniel.schmidt0204@gmail.com>
2025-02-02 14:16:51 +0100

    new telescope layou; added noice impl

--- lua/plugins/telescope.lua
+++ lua/plugins/telescope.lua
@@ -6,11 +6,10 @@ return {
             'nvim-lua/plenary.nvim',
             'nvim-tree/nvim-web-devicons',
             'folke/todo-comments.nvim',
-            --{ 'nvim-telescope/telescope-fzf-native.nvim', build = "make"},
         },
         config = function()
             local telescope = require("telescope")
-
+            local actions = require("telescope.actions")
 
             telescope.setup({
                 defaults = {
@@ -18,6 +17,23 @@ return {
                     file_ignore_patterns = {
                         "target"
                     },
+                    layout_strategy = "vertical",
+                    layout_config = {
+                        width = 0.99,
+                        height = 0.99,
+                        prompt_position = "bottom",
+                    },
+                    -- border = false,
+                    mappings = {
+                        i = {
+                            ["<C-Up>"] = actions.preview_scrolling_up,
+                            ["<C-Down>"] = actions.preview_scrolling_down,
+                        },
+                        n = {
+                            ["<C-Up>"] = actions.preview_scrolling_up,
+                            ["<C-Down>"] = actions.preview_scrolling_down,
+                        },
+                    }
                 },
             })
 
@@ -26,6 +42,7 @@ return {
             keymap("n", "<leader>sf", "<cmd>Telescope find_files<cr>", { desc = "Search files"})
             keymap("n", "<leader>sr", "<cmd>Telescope live_grep<cr>", { desc = "Live grep"})
             keymap("n", "<leader>sh", "<cmd>Telescope help_tags<cr>", { desc = "Live grep"})
+            keymap("n", "<leader>sn", "<cmd>Telescope noice<cr>", { desc = "Search notifications"})
         end
     }
 }
