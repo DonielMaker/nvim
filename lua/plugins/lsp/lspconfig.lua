9ee5161ef2a0b62b3a55682040b419b6e14a21ec
donielmaker <daniel.schmidt0204@gmail.com>
2025-02-19 12:02:34 +0100

    added marksman

--- lua/plugins/lsp/lspconfig.lua
+++ lua/plugins/lsp/lspconfig.lua
@@ -16,6 +16,7 @@ return {
 
         opts = {
             servers = {
+                marksman = {},
                 texlab = {},
                 lua_ls = {},
                 rust_analyzer = {},
