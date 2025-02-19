24e0180c075a991756953fc216dd032d0cdd17fa
donielmaker <daniel.schmidt0204@gmail.com>
2025-02-19 12:02:48 +0100

    moved from image.nvim to the snacks.nvim impl

---
+++ lua/plugins/snacks.lua
@@ -0,0 +1,12 @@
+return {
+    "folke/snacks.nvim",
+    -- --@type snacks.Config
+    priority = 1000,
+    opts = {
+        image = {
+            -- your image configuration comes here
+            -- or leave it empty to use the default settings
+            -- refer to the configuration section below
+        }
+    }
+}
