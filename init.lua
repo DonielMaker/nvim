9d41be7ede4bfa61f5e50620c1b4dadfaf5fd6f4
donielmaker <daniel.schmidt0204@gmail.com>
2025-10-08 19:32:30 +0200

    added lspconfig and blink

--- init.lua
+++ init.lua
@@ -28,20 +28,24 @@ vim.o.splitbelow = true -- split horizontal window to the bottom
 
 vim.pack.add({
     {src = "https://github.com/catppuccin/nvim"},
+    {src = "https://github.com/nvim-tree/nvim-web-devicons"},
     {src = "https://github.com/windwp/nvim-autopairs"},
     {src = "https://github.com/windwp/nvim-ts-autotag"},
     {src = "https://github.com/nvim-lualine/lualine.nvim"},
     {src = "https://github.com/nvim-treesitter/nvim-treesitter"},
     {src = "https://github.com/folke/which-key.nvim"},
     {src = "https://github.com/folke/trouble.nvim"},
-    -- {src = "https://github.com/saghen/blink.cmp"},
-    {src = "https://github.com/neovim/nvim-lspconfig"},
     {src = "https://github.com/ibhagwan/fzf-lua"},
     {src = "https://github.com/stevearc/oil.nvim"},
-    {src = "https://github.com/nvim-tree/nvim-web-devicons"},
+    {src = "https://github.com/folke/lazydev.nvim"},
+    {src = "https://github.com/neovim/nvim-lspconfig"},
+    {src = "https://github.com/saghen/blink.cmp"},
+    {src = "https://github.com/stevearc/dressing.nvim"},
 })
 
 vim.cmd.colorscheme "catppuccin-macchiato"
+
+require("nvim-web-devicons").setup()
 
 require("nvim-ts-autotag").setup({
     opts = {
@@ -80,17 +84,20 @@ require("nvim-treesitter.configs").setup({
     },
 })
 
+
+require("which-key").setup({
+    preset = "modern",
+
+    win = { border = "rounded" },
+})
 require("which-key").add({
-    { "<leader>s", group = "Search"},
+    { "<leader>s", group = "Search", icon = ""},
     { "<leader>f", group = "File explorer"},
-    { "<leader>c", group = "Content"},
-    { "<leader>x", group = "Trouble"},
-    { "<leader>t", group = "Panes"},
+    { "<leader>x", group = "Trouble", icon = ""},
+    { "<leader>t", group = "Panes", icon = ""},
 })
 
 require("trouble").setup({ focus = true, })
-
--- require("blink.cmp").setup({})
 
 require("fzf-lua").setup({
     winopts = {
@@ -130,14 +137,79 @@ require("oil").setup({
     },
 })
 
-vim.keymap.set("n", "<leader>ff", "<CMD>Oil<CR>", {desc = "Open parent dir"})
+require("lazydev").setup()
 
-vim.keymap.set("n", "<Esc>", "<CMD>nohl<CR>", { desc = "Clear search highlights" })
+-- Create all the left side icons based on their severity
+vim.diagnostic.config({
+    signs = {
+        text = {
+            [vim.diagnostic.severity.ERROR] = " ",
+            [vim.diagnostic.severity.WARN] = " ",
+            [vim.diagnostic.severity.HINT] = "󰠠 ",
+            [vim.diagnostic.severity.INFO] = " ",
+        }
+    }
+})
 
-vim.keymap.set("n", "<leader>ss", "<CMD>FzfLua<CR>", { desc = "FzfLua" })
-vim.keymap.set("n", "<leader>sf", "<CMD>FzfLua files<CR>", { desc = "Search Files" })
-vim.keymap.set("n", "<leader>sr", "<CMD>FzfLua grep<CR>", { desc = "Grep Files" })
-vim.keymap.set("n", "<leader>sc", "<CMD>FzfLua files cwd=~/.config<CR>", { desc = "Search .config" })
+local servers = {
+    marksman = {},
+    tinymist = {},
+    yamlls = {},
+    nixd = {},
+    lua_ls = {},
+    rust_analyzer = {},
+    vtsls = {},
+    cssls = {
+        settings = {
+            css = { validate = true; },
+            scss = { validate = true; },
+            less = { validate = true; },
+        }
+    },
+    bashls = {},
+    qmlls = {},
+}
+
+-- Create lsp based on settings defined in servers
+for server, config in pairs(servers) do
+    vim.lsp.config(server, config)
+    vim.lsp.enable(server)
+end
+
+
+require("blink.cmp").setup({
+
+    keymap = {
+        ["<Up>"] = {"select_prev", "fallback"},
+        ["<Down>"] = {"select_next", "fallback"},
+        ["<C-Space>"] = {"show", "show_documentation", "hide_documentation"},
+        ["<C-Enter>"] = {"select_and_accept", "fallback"},
+    },
+
+    appearance = {
+        nerd_font_variant = 'normal'
+    },
+
+    completion = {
+        documentation = { auto_show = false },
+
+        ghost_text = {enabled = false},
+    },
+
+    signature = { enabled = true },
+
+    fuzzy = { implementation = "lua" },
+})
+
+-- Keybinds
+vim.keymap.set("n", "<leader>ff", "<cmd>Oil<cr>", {desc = "Open parent dir"})
+
+vim.keymap.set("n", "<Esc>", "<cmd>nohl<cr>", { desc = "Clear search highlights" })
+
+vim.keymap.set("n", "<leader>ss", "<cmd>FzfLua<cr>", { desc = "FzfLua" })
+vim.keymap.set("n", "<leader>sf", "<cmd>FzfLua files<cr>", { desc = "Search Files" })
+vim.keymap.set("n", "<leader>sr", "<cmd>FzfLua grep<cr>", { desc = "Grep Files" })
+vim.keymap.set("n", "<leader>sc", "<cmd>FzfLua files cwd=~/.config<cr>", { desc = "Search .config" })
 
 vim.keymap.set("n", "<leader>tv", "<C-w>v", { desc = "Split window vertically"})
 vim.keymap.set("n", "<leader>th", "<C-w>s", { desc = "Split window horizontally"})
@@ -148,5 +220,5 @@ vim.keymap.set("n", "<C-j>", "<C-w>j", { desc = "Navigate Down"})
 vim.keymap.set("n", "<C-h>", "<C-w>h", { desc = "Navigate Left"})
 vim.keymap.set("n", "<C-l>", "<C-w>l", { desc = "Navigate Right"})
 
-vim.keymap.set("n", "<leader>xd", "<cmd>Trouble diagnostics filter.buf=0<CR>", { desc = "Open trouble document diagnostics"})
+vim.keymap.set("n", "<leader>xd", "<cmd>Trouble diagnostics filter.buf=0<cr>", { desc = "Open trouble document diagnostics"})
 -- keymap("n", "<leader>dd", function() require("snacks").dashboard() end, { desc = "load the dashboard"})
